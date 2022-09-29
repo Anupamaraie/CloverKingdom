8aa3f0787a68042b895d94de81ebde179713471f
Anupamaraie <anuraie1@gmail.com>
2022-09-29 09:25:48 +0545

    Clover

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Contact(models.Model):
+    name= models.CharField(max_length=122)
+    kingdom = models.CharField(max_length = 50)
+    squad = models.CharField(max_length=100)
+    help = models.TextField()
+    
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from datetime import date, datetime
+from multiprocessing import reduction
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from app.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":
+        name=request.POST.get("name")
+        kingdom=request.POST.get("kingdom")
+        squad=request.POST.get("squad")
+        help=request.POST.get("help")
+        obj = Contact(name=name,kingdom=kingdom,squad=squad,help=help)
+        obj.save()
+        messages.success(request,"Submitted!!")
+        return redirect('/#contact')
     return render(request,'index.html')
